93d94c4cd1156e8ee578ecefa9c3c57075bf11fd
Sridhar Devarakonda <devarakonda.mitmpl2025@learner.manipal.edu>
2026-07-14 15:31:04 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long;
+ 
+void solve(){
+    int m,n;
+    cin>>m>>n;
+    int d = (m*n)/2;
+    cout<<d;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+}
