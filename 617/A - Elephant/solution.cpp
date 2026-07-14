58fda0300f610c58d33289ef34fd23cbca1aacda
Sridhar Devarakonda <devarakonda.mitmpl2025@learner.manipal.edu>
2026-07-14 15:30:28 +0530

    Add Elephant [A] from Codeforces

---
+++ 617/A - Elephant/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long;
+ 
+void solve(){
+    int x;
+    cin>>x;
+    int d = (x+5-1)/5;
+    cout<<d;
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+}
